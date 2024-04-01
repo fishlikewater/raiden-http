4eb6923a915e6dd9f5fc4eaefca6c5505c87f670
zhangxiang <zhangxiang@uphicoo.com>
2024-04-01 09:06:40 +0800

    feat: 参数检测

--- raiden-core/src/main/java/com/github/fishlikewater/raidencore/processor/CheckProcessor.java
+++ raiden-core/src/main/java/com/github/fishlikewater/raidencore/processor/CheckProcessor.java
@@ -17,6 +17,7 @@ package com.github.fishlikewater.raidencore.processor;
 
 import com.github.fishlikewater.raidencore.HeadWrap;
 import com.github.fishlikewater.raidencore.annotation.Heads;
+
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
@@ -65,32 +66,13 @@ public class CheckProcessor extends AbstractProcessor {
         if (typeMirror instanceof DeclaredType declaredType) {
             List<? extends TypeMirror> typeArguments = declaredType.getTypeArguments();
             if (declaredType.asElement().toString().equals(Map.class.getName())) {
-                TypeMirror keyType = typeArguments.get(0);
-                TypeMirror valueType = typeArguments.get(1);
-
-                // 这里只是一个简单的判断示例，你可以根据实际需求进一步检查
-                boolean isKeyTypeString = keyType.toString().equals(String.class.getName());
-                boolean isValueTypeObject = valueType.toString().equals(String.class.getName());
-                if (isKeyTypeString && isValueTypeObject) {
-                    return true;
-                } else {
-                    this.processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "只支持Map<String, String>与HeadWrap参数类型", element);
-                    return false;
-                }
+                return this.handleMap(element, typeArguments);
             }
             return declaredType.asElement().toString().equals(HeadWrap.class.getName());
         }
         return false;
     }
 
-
-    /**
-     * 这个方法虽然在父类当中不是 abstract的，但是我们也必须实现。
-     * 因为该方法的作用是指定我们要处理哪些注解的，
-     * 比如你想处理注解MyAnnotation,可是该处理器怎么知道你想处理MyAnnotation，而不是OtherAnnotation呢。
-     * 所以你要在这里指明，你需要处理的注解的全称。
-     * 返回值是一个字符串的集合，包含着本处理器想要处理的注解类型的合法全称。
-     */
     @Override
     public Set<String> getSupportedAnnotationTypes() {
         Set<String> strings = new TreeSet<>();
@@ -98,12 +80,22 @@ public class CheckProcessor extends AbstractProcessor {
         return strings;
     }
 
-    /**
-     * 本方法用来指明你支持的java版本，
-     * 不过一般使用 SourceVersion.latestSupported() 就可以了。
-     */
     @Override
     public SourceVersion getSupportedSourceVersion() {
         return SourceVersion.latestSupported();
+    }
+
+    private boolean handleMap(Element element, List<? extends TypeMirror> typeArguments) {
+        TypeMirror keyType = typeArguments.get(0);
+        TypeMirror valueType = typeArguments.get(1);
+
+        boolean isKeyTypeString = keyType.toString().equals(String.class.getName());
+        boolean isValueTypeObject = valueType.toString().equals(String.class.getName());
+        if (isKeyTypeString && isValueTypeObject) {
+            return true;
+        } else {
+            this.processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "只支持Map<String, String>与HeadWrap参数类型", element);
+            return false;
+        }
     }
 }
